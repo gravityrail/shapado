00ff3db70b88311e2cdaea2c51ef1da83989a392
David A. Cuadrado <krawek@gmail.com>
2010-10-30 19:25:48 -0500

    add login_required method
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -266,4 +266,18 @@ class ApplicationController < ActionController::Base
     "/_files/groups/favicon/#{group.id}"
   end
   helper_method :favicon_group_path
+
+
+  def login_required
+    respond_to do |format|
+      format.js do
+        if warden.authenticate(:scope => :user).nil?
+          return render(:json => {:message => t("global.please_login"),
+                                            :success => false,
+                                            :status => :unauthenticate}.to_json)
+        end
+      end
+      format.any { warden.authenticate!(:scope => :user) }
+    end
+  end
 end
