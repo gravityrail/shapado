75beb5850160ef02e5bf8d5cf0560ae0b82881f4
Patrick Aljord <patcito@gmail.com>
2010-11-05 20:18:04 -0500

    only ask for location when focus on textarea

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -203,10 +203,12 @@ function supports_input_placeholder() {
 
 function init_geolocal(){
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-        $('.lat_input').val(position.coords.latitude)
-        $('.long_input').val(position.coords.longitude)
-    }, function(){});
+    $('textarea, #question_title').live('focus', function(){
+      navigator.geolocation.getCurrentPosition(function(position){
+          $('.lat_input').val(position.coords.latitude)
+          $('.long_input').val(position.coords.longitude)
+      }, function(){});
+    });
   } else {
       //error('not supported');
   }
