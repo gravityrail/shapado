8c57500e6cb14ecf062ab375418e76d04b5fcbb9
David A. Cuadrado <krawek@gmail.com>
2010-11-09 02:10:49 -0500

    fix flags
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -9,6 +9,8 @@ class Flag
   field :user_id, :type => String
   referenced_in :user
 
+  embedded_in :flaggable, :inverse_of => :flags
+
   validates_presence_of :user
   validates_inclusion_of :reason, :in => REASONS
 
@@ -17,7 +19,7 @@ class Flag
 
   protected
   def should_be_unique
-    request = self._root_document.flags.detect{ |rq| rq.user_id == self.user_id }
+    request = self.flaggable.flags.detect{ |rq| rq.user_id == self.user_id }
     valid = (request.nil? || request.id == self.id)
 
     if !valid
@@ -28,8 +30,8 @@ class Flag
   end
 
   def check_reputation
-    if ((self._root_document.user_id == self.user_id) && !self.user.can_flag_on?(self._root_document.group))
-      reputation = self._root_document.group.reputation_constrains["flag"]
+    if ((self.flaggable.user_id == self.user_id) && !self.user.can_flag_on?(self.flaggable.group))
+      reputation = self.flaggable.group.reputation_constrains["flag"]
       self.errors.add(:reputation, I18n.t("users.messages.errors.reputation_needed",
                                           :min_reputation => reputation,
                                           :action => I18n.t("users.actions.flag")))
