8cb3f12cf51ba799be0cac3c8e2680359e4911bc
David A. Cuadrado <krawek@gmail.com>
2010-11-09 01:21:40 -0500

    only enable exception notification on prod env
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,7 +1,7 @@
 class Error404 < Exception
 end
 
-if AppConfig.exception_notification['activate']
+if AppConfig.exception_notification['activate'] && Rails.env == "production"
   Shapado::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[Shapado exception] ",
   :sender_address => AppConfig.exception_notification['exception_sender_address'],
