e5a1ec94ce11f0caa99b6d7824fffec86bbba46b
David A. Cuadrado <krawek@gmail.com>
2010-11-09 02:16:53 -0500

    fix close requests
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/close_request.rb
+++ app/models/close_request.rb
@@ -8,6 +8,7 @@ class CloseRequest
   field :comment, :type => String
 
   referenced_in :user
+  embedded_in :closeable, :inverse_of => :close_requests
 
   validates_presence_of :user
 
@@ -15,16 +16,16 @@ class CloseRequest
   validate :check_reputation
 
   def increment_counter
-    self._root_document.increment(:close_requests_count => 1)
+    self.closeable.increment(:close_requests_count => 1)
   end
 
   def decrement_counter
-    self._root_document.decrement(:close_requests_count => 1)
+    self.closeable.decrement(:close_requests_count => 1)
   end
 
   protected
   def should_be_unique
-    request = self._root_document.close_requests.detect{ |rq| rq.user_id == self.user_id }
+    request = self.closeable.close_requests.detect{ |rq| rq.user_id == self.user_id }
     valid = (request.nil? || request.id == self.id)
 
     unless valid
@@ -35,16 +36,16 @@ class CloseRequest
   end
 
   def check_reputation
-    if ((self._root_document.user_id == self.user_id) && !self.user.can_vote_to_close_own_question_on?(self._root_document.group))
-      reputation = self._root_document.group.reputation_constrains["vote_to_close_own_question"]
+    if ((self.closeable.user_id == self.user_id) && !self.user.can_vote_to_close_own_question_on?(self.closeable.group))
+      reputation = self.closeable.group.reputation_constrains["vote_to_close_own_question"]
       self.errors.add(:reputation, I18n.t("users.messages.errors.reputation_needed",
                                           :min_reputation => reputation,
                                           :action => I18n.t("users.actions.vote_to_close_own_question")))
       return false
     end
 
-    unless self.user.can_vote_to_close_any_question_on?(self._root_document.group)
-      reputation = self._root_document.group.reputation_constrains["vote_to_close_any_question"]
+    unless self.user.can_vote_to_close_any_question_on?(self.closeable.group)
+      reputation = self.closeable.group.reputation_constrains["vote_to_close_any_question"]
             self.errors.add(:reputation, I18n.t("users.messages.errors.reputation_needed",
                                           :min_reputation => reputation,
                                           :action => I18n.t("users.actions.vote_to_close_any_question")))
