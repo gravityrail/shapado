1f4aa2091c203ef1da781729e802de8c51d22228
David A. Cuadrado <krawek@gmail.com>
2010-11-14 15:03:52 -0500

    new EmbeddedHash which fakes some Mongoid::Document methods; fixes memberships
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ lib/embedded_hash.rb
@@ -0,0 +1,20 @@
+class EmbeddedHash < Hash
+  include ActiveModel::Validations
+
+  def initialize(other = {})
+    other.each do |k,v|
+      self[k] = v
+    end
+    self["_id"] ||= BSON::ObjectId.new.to_s
+  end
+
+  def self.field(name, opts = {})
+    define_method(name) do
+      self[name.to_s] ||= opts[:default].kind_of?(Proc) ? opts[:default].call : opts[:default]
+    end
+
+    define_method("#{name}=") do |v|
+      self[name.to_s] = v
+    end
+  end
+end
