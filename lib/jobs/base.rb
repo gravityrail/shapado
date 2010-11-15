fc91edda4337143ca4bf7dda8eca13b709c74be1
David A. Cuadrado <krawek@gmail.com>
2010-11-14 21:51:15 -0500

    fix on_create_answer task
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/jobs/base.rb
+++ lib/jobs/base.rb
@@ -21,7 +21,7 @@ module Jobs
       else
         user.increment(:"membership_list.#{group.id}.#{badge.type}_badges_count" => 1)
         if badge.token == "editor"
-          user.set(:"membership_list.#{group.id}.is_editor" => true)
+          user.override(:"membership_list.#{group.id}.is_editor" => true)
         end
       end
 
@@ -30,6 +30,7 @@ module Jobs
         if !user.email.blank? && user.notification_opts.activities
           Notifier.earned_badge(user, group, badge).deliver
         end
+
         if user.notification_opts.badges_to_twitter
           token = badge.name(user.language)
           group_name = group.name
