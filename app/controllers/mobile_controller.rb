d84b13d19a9dbb15d8e54f94f6004111486a852e
David A. Cuadrado <krawek@gmail.com>
2010-11-27 17:48:22 -0500

    a couple of fixes
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -2,7 +2,7 @@ class MobileController < ApplicationController
   layout 'mobile'
 
   def index
-    @questions = current_group.questions.paginate(:per_page => params[:per_page]||25, :page => params[:page])
+    @questions = current_group.questions.order_by(:created_at.desc).paginate(:per_page => params[:per_page]||25, :page => params[:page])
   end
 
 end
