d94bdb8bdd6acd2e6696ad6e66647b1d8c81bd15
Francisco Rodriguez <dgfranciscorodriguez@gmail.com>
2010-11-30 10:26:24 -0500

    More actions menu for questions in place - still need a bit of love for icons, but its working
    Signed-off-by: Francisco Rodriguez <dgfranciscorodriguez@gmail.com>

--- public/javascripts/modules/effects.js
+++ public/javascripts/modules/effects.js
@@ -1,8 +1,18 @@
-// Efectos 
+// Efects 
 
+// Open and show a panel
 $(document).ready(function(){
 	$(".btn-slide").click(function(){
 		$("#panel").slideToggle("slow");
 		$(this).toggleClass("active"); return false;
   });	 
 });
+
+// Navs, drop down menus
+jQuery(function(){
+	jQuery('ul.drop-menu').superfish({
+    hoverClass:    'dropHover',
+    autoArrows:    false,
+  });
+});
+
