583c731f2f62ace8effe9fe47b469bbea4a9e31a
David A. Cuadrado <krawek@gmail.com>
2010-12-05 22:31:13 -0500

    javascript to update internal links [mobile]
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
+  $('div[data-role*="page"]').live('pageshow',function(event, ui){
+    var internal = $("a").filter(function() {
+      return (this.hostname == location.hostname && !this.href.match("mobile"));
+    });
+
+    $.each(internal, function() {
+      var link = $(this);
+      var href = link.attr("href");
+
+      if(href.match('\\?')) {
+        link.attr("href", href+"&format=mobile");
+      } else {
+        link.attr("href", href+"?format=mobile");
+      }
+    })
+  });
 });
