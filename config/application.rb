8c9959e3b4ec6cdb02b5e218684475ae63c36283
Patrick Aljord <patcito@gmail.com>
2011-03-23 01:38:59 -0500

    suggest tags followed by suggested friends

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+
 #require 'goalie/rails'
 
 # If you have a Gemfile, require the gems listed there, including any gems
