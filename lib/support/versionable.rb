29f54508ee3dbc4ec5116346da37e57a66dd8afe
David A. Cuadrado <krawek@gmail.com>
2011-04-17 22:32:12 -0500

    reimplement versions backend
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/support/versionable.rb
+++ lib/support/versionable.rb
@@ -4,24 +4,21 @@ module Versionable
     klass.class_eval do
       extend ClassMethods
       include InstanceMethods
+
       attr_accessor :rolling_back
       field :version_message
-      embeds_many :versions
+
+      field :versions_count, :type => Integer, :default => 0
+      field :version_ids, :type => Array
 
       before_save :save_version, :if => Proc.new { |d| !d.rolling_back }
-
-      alias_method :assign_versions, :versions=
-      define_method(:versions=) do |v|
-        return if v.nil?
-        assign_versions v
-      end
     end
   end
 
   module InstanceMethods
     def rollback!(pos = nil)
-      pos = self.versions.count-1 if pos.nil?
-      version = self.versions[pos]
+      pos = self.versions_count-1 if pos.nil?
+      version = self.version_at(pos)
 
       if version
         version.data.each do |key, value|
@@ -36,8 +33,8 @@ module Versionable
     end
 
     def load_version(pos = nil)
-      pos = self.versions.count-1 if pos.nil?
-      version = self.versions[pos]
+      pos = self.versions_count-1 if pos.nil?
+      version = self.version_at(pos)
 
       if version
         version.data.each do |key, value|
@@ -62,16 +59,64 @@ module Versionable
       when :current
         current_version
       when :first
-        self.versions.first
+        version_klass.find(self.version_ids.first)
       when :last
-        self.versions.last
+        version_klass.find(self.version_ids.last)
       else
-        self.versions[pos]
+        version_klass.find(self.version_ids[pos])
       end
+    end
+
+    def versions
+      version_klass.where(:target_id => self.id)
+    end
+
+    def version_klass
+      self.class.version_klass
     end
   end
 
   module ClassMethods
+    def version_klass
+      parent_klass = self
+      @version_klass ||= Class.new do
+        include Mongoid::Document
+        include Mongoid::Timestamps
+
+        cattr_accessor :parent_class
+        self.parent_class = parent_klass
+
+        self.collection_name = "#{self.parent_class.collection_name}.versions"
+
+        identity :type => String
+        field :message, :type => String
+        field :data, :type => String
+        field :user_id, :type => String
+        referenced_in :user
+
+        referenced_in :target, :polymorphic => true
+
+        after_create :add_version
+
+        validates_presence_of :target_id
+
+        def content(key)
+          cdata = self.data[key]
+          if cdata.respond_to?(:join)
+            cdata.join(" ")
+          else
+            cdata || ""
+          end
+        end
+
+        private
+        def add_version
+          self.class.parent_class.push({:_id => self.target_id}, {:version_ids => self.id})
+          self.class.parent_class.increment({:_id => self.target_id}, {:versions_count => 1})
+        end
+      end
+    end
+
     def versionable_keys(*keys)
       define_method(:save_version) do
         data = {}
@@ -91,11 +136,12 @@ module Versionable
         end
 
         if !self.new? && !data.empty? && self.updated_by_id
-          e = Time.now
-          self.versions << Version.new({'data' => data,
-                                        'user_id' => (self.updated_by_id_was || self.updated_by_id),
-                                        'date' => e.kind_of?(ActiveSupport::TimeWithZone) ? e.utc : e,
-                                        'message' => message})
+          self.version_klass.create({
+            'data' => data,
+            'user_id' => (self.updated_by_id_was || self.updated_by_id),
+            'target' => self,
+            'message' => message
+          })
         end
       end
 
