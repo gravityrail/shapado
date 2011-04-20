4d157f8c1de0e246782273670d65250334d0c4b4
Siebrand Mazeland <s.mazeland@xs4all.nl>
2011-04-20 10:07:39 +0200

    Localisation updates from http://translatewiki.net.

---
+++ public/javascripts/i18n/ms.js
@@ -0,0 +1,11 @@
+/** Messages for Malay (Bahasa Melayu)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Anakmalaysia
+ */
+
+var I18n = {
+	on_leave_page: "Anda mungkin kehilangan pengubahan anda"
+};
+
