25234f6e16c6e13af9987d00338f16cc1ad1f12e
David A. Cuadrado <krawek@gmail.com>
2011-05-12 18:50:16 -0500

    more scheduler fixes
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,11 @@
 require 'bundler'
 APP_ROOT = Bundler.root.to_s
+launcher = "cd #{APP_ROOT}; rvm 1.9.2 exec bundle exec"
 
-daily_report = "#{APP_ROOT}/script/daily_report production"
-cleanup = "#{APP_ROOT}/script/cleanup production"
-twitter = "#{APP_ROOT}/script/import_twitter production"
-email = "#{APP_ROOT}/script/import_email production"
+daily_report = "#{launcher} script/daily_report production"
+cleanup = "#{launcher} script/cleanup production"
+twitter = "#{launcher} script/import_twitter production"
+email = "#{launcher} script/import_email production"
 
 env "PATH", ENV["PATH"]
 set :output, "#{APP_ROOT}/log/crontab.log"
