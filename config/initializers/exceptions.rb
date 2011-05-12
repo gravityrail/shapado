5c95440c8e6ed060db6ef2f8ce84c0858eaf7a8d
David A. Cuadrado <krawek@gmail.com>
2011-05-12 12:36:48 -0500

    use the application name when reporting errors
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -3,7 +3,7 @@ end
 
 if AppConfig.exception_notification['activate'] && Rails.env == "production"
   Shapado::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[Shapado exception] ",
+  :email_prefix => "[#{AppConfig.application_name} Exception] ",
   :sender_address => AppConfig.exception_notification['exception_sender_address'],
   :exception_recipients => AppConfig.exception_notification['exception_recipients']
 end
