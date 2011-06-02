417182c520f55450ff4726b1bdebe597bef8f983
David A. Cuadrado <krawek@gmail.com>
2011-06-02 16:42:19 -0500

    add skel for ui handling from javascript
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ public/javascripts/shapado_ui/answer_ui.js
@@ -0,0 +1,12 @@
+var AnswerUI = {
+  create_on_index: function(data) {
+  },
+  create_on_show: function(data) {
+  },
+  update_on_index: function(data) {
+
+  },
+  update_on_show: function(data) {
+
+  }
+}
---
+++ public/javascripts/shapado_ui/shapado_ui.js
@@ -0,0 +1,9 @@
+var ShapadoUI = {
+  new_question: function(data) {
+
+  },
+  update_question: function(data) {
+  }
+};
+
+
