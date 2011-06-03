8abe3b6fccc21842b941c04c096ca903b06b2d76
David A. Cuadrado <krawek@gmail.com>
2011-06-03 00:05:17 -0500

    fix import/export scripts
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/export.rb
+++ app/models/export.rb
@@ -7,17 +7,17 @@ class Export
     $stderr.puts ">> Exporting model #{model}"
 
     if model.kind_of?(MongoMapper::Document)
-      io.write(model.to_json(opts)+"\n")
+      io.write(model.attributes.to_json(opts)+"\n")
     elsif model.respond_to?(:klass)
       model.all.each do |object|
-        io.write object.to_json({:except => [:_keywords]}.merge(opts)) + "\n"
+        io.write object.attributes.to_json({:except => [:_keywords]}.merge(opts)) + "\n"
       end
     else
       selector = opts.delete(:selector) || {:group_id => @group.id}
 
       puts "selector=#{selector.inspect}"
       model.where(selector).all.each do |object|
-        io.write object.to_json({:except => [:_keywords]}.merge(opts)) + "\n"
+        io.write object.attributes.to_json({:except => [:_keywords]}.merge(opts)) + "\n"
       end
     end
   end
