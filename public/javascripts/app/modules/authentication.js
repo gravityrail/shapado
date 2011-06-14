27bdcaa6629eb8bb6a5167a26fe15fc9f6ec4bf1
David A. Cuadrado <krawek@gmail.com>
2011-06-14 01:58:51 -0500

    refactor this so we can use it everywhere
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- public/javascripts/app/modules/authentication.js
+++ public/javascripts/app/modules/authentication.js
@@ -2,11 +2,16 @@ var Auth = {
   initialize: function() {
     $('.auth-provider').click(function(){
       var authUrl = $(this).attr('href');
-      $.cookie('pp', 1);
-      var pparg;
-      (authUrl.indexOf('?')==-1)? pparg = '?pp=1' : pparg = '&pp=1'
-      window.open(authUrl+pparg, 'openid_popup', 'width=700,height=500');
+
+      Auth.open_popup(authUrl);
+
       return false;
     });
+  },
+  open_popup: function(authUrl) {
+    $.cookie('pp', 1);
+    var pparg;
+    (authUrl.indexOf('?')==-1)? pparg = '?pp=1' : pparg = '&pp=1'
+    window.open(authUrl+pparg, 'openid_popup', 'width=700,height=500');
   }
 };
