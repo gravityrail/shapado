a60471ede198e69d361d0810400a2c93ee82373b
David A. Cuadrado <krawek@gmail.com>
2011-06-14 01:59:13 -0500

    improve facebook app
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- public/javascripts/facebook_app/init.js
+++ public/javascripts/facebook_app/init.js
@@ -1,5 +1,16 @@
-var Questions = {
+var FbQuestions = {
   initialize: function() {
+    if($(".unauthenticated").length > 0) {
+      $("input, textarea").focus(function() {
+        Auth.open_popup('/users/auth/facebook');
+      });
+
+      $(".require_login").click(function() {
+        Auth.open_popup('/users/auth/facebook');
+        return false;
+      });
+    }
+
     $("article.Question h3 a").click(function() {
       var l = $(this);
       var q = l.parents("article.Question");
@@ -8,9 +19,46 @@ var Questions = {
 
       return false;
     });
+
+    $(".quick_question form").submit(function() {
+      var f = $(this);
+      var step1 = f.find(".step1");
+      var step2 = f.find(".step2");
+
+      var href = f.attr("action");
+
+      if(!step2.is(':visible')) {
+        step2.slideDown();
+      } else {
+        $.ajax({
+          url: href+'.js',
+          dataType: 'json',
+          type: "POST",
+          data: f.serialize()+"&facebook=1",
+          success: function(data){
+            if(data.success){
+              step2.slideUp();
+
+              Messages.show(data.message, "notice");
+              $("section.questions-index").prepend(data.html);
+            } else {
+              Messages.show(data.message, "error");
+
+              if(data.status == "unauthenticate") {
+                Auth.open_popup("/users/auth/facebook");
+              }
+            }
+          },
+          error: Messages.ajax_error_handler,
+          complete: function(XMLHttpRequest, textStatus) {
+          }
+        });
+      }
+      return false;
+    });
   }
 };
 
 $(document).ready(function() {
-  Questions.initialize();
+  FbQuestions.initialize();
 });
