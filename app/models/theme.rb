930e5d6a4d97a67f4cbfb0337a3963e4e17fa834
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2011-06-23 16:43:24 -0500

    Use theme's properties as sass var for the theme's template
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -36,8 +36,26 @@ class Theme
   def generate_stylesheet
     css = StringIO.new
     template_file = File.join(Rails.root,"lib","sass","theme_template.scss")
-    css = Sass.compile(File.read(template_file), {:style => :compressed})
+    css = Sass.compile(define_vars << File.read(template_file), {:style => :compressed})
     css << self.custom_css || ""
     self.stylesheet = css
+  end
+
+  def define_vars
+    %@
+$has_bg_image: #{self.has_bg_image?};
+$bg_color: ##{self.bg_color};
+$fg_color: ##{self.fg_color};
+
+$view_bg_color: ##{self.view_bg_color};
+$view_fg_color: ##{self.view_fg_color};
+
+$button_bg_color: ##{self.button_bg_color};
+$button_fg_color: ##{self.button_fg_color};
+
+$use_link_bg_color: #{self.use_link_bg_color};
+$link_bg_color: ##{self.link_bg_color};
+$link_fg_color: ##{self.link_fg_color};
+@
   end
 end
