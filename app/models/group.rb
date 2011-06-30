c490dff1d2adff06cb8ddb258aa3638f62c019b2
sharp <sharp@sharp-Rev-1-0.(none)>
2011-06-30 14:17:15 +0800

    fix theme id error when creating group

--- app/models/group.rb
+++ app/models/group.rb
@@ -422,6 +422,7 @@ class Group
   end
 
   def set_default_theme
-    self.current_theme_id = Theme.where(:is_default => true).only(:_id).first
+    self.current_theme_id = Theme.where(:is_default => true).only(:_id).first.id
+  
   end
 end
