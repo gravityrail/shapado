45a5f4271eed86b29d419ce17ac9e190082e0d0f
David A. Cuadrado <krawek@gmail.com>
2011-07-02 15:57:40 -0500

    implemented layout editor
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  AppConfig.initialize();
   Geo.initialize();
   LocalStorage.initialize();
   Tags.initialize();
   Votes.initialize();
   Notifier.initialize();
   Networks.initialize();
+  LayoutEditor.initialize();
 });
