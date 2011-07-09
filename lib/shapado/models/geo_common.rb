1ef60d777af6363b3019c37e78fbcdabab30b9c0
Patrick Aljord <patcito@gmail.com>
2011-07-09 18:51:53 -0500

    replace openstreetmap with mapquest, same data but mapquest has unlimited quota

--- lib/shapado/models/geo_common.rb
+++ lib/shapado/models/geo_common.rb
@@ -19,30 +19,30 @@ module Models
     module InstanceMethods
 
     def set_address
-        lat = self["position"]["lat"]
-        long = self["position"]["long"]
-        if lat != 0 || long != 0
-          self["address"] = Nominatim::Place.new(lat, long).get_address
-          self.save
-          if self.user.address != self.address
-            self.user.position = self.position
-            self.user.address = self.address
-            self.user.save
-          end
+      lat = self["position"]["lat"]
+      long = self["position"]["long"]
+      if lat != 0 || long != 0
+        self["address"] = Nominatim::Place.new(lat, long).get_address
+        self.save
+        if self.user.address != self.address
+          self.user.position = self.position
+          self.user.address = self.address
+          self.user.save
         end
       end
+    end
 
-      def address_name
-        address = if self.address.present? && self.address != { }
-          unless self.address["city"].blank?
-            "#{self.address["city"]}, #{self.address["country"]}"
-          else
-            self.address["country"]
-          end
-        else
-          I18n.t('global.unknown_place')
-        end
-      end
+    def address_name
+      address = if self.address.present? && self.address != { }
+                  unless self.address["city"].blank?
+                    "#{self.address["city"]}, #{self.address["country"]}"
+                  else
+                    self.address["country"]
+                  end
+                else
+                  I18n.t('global.unknown_place')
+                end
+    end
 
       def point(max_distance=6)
         @_point ||= self.position.merge({:$maxDistance=>6})
