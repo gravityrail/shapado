ce4d3b143098d73feaff50ca5b250042e9f9b376
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2011-07-17 15:01:29 -0500

    Fix activities filters
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,6 +5,17 @@ class ActivitiesController < ApplicationController
       conds[:user_id] = current_user.id
     end
 
+    case params[:tab]
+    when "questions"
+      conds[:trackable_type] = "Question"
+    when "answers"
+      conds[:trackable_type] = "Answer"
+    when "users"
+      conds[:trackable_type] = "User"
+    when "pages"
+      conds[:trackable_type] = "Page"
+    end
+
     @activities = current_group.activities.where(conds).order(:created_at.desc).
                                            paginate(paginate_opts(params))
 
