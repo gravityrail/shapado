de9bdbd56c86f3235ec40d36272a7bcd0f482037
Patrick Aljord <patcito@gmail.com>
2011-07-26 17:36:03 -0500

    disallow js on update too

--- app/models/group.rb
+++ app/models/group.rb
@@ -140,6 +140,7 @@ class Group
   validates_inclusion_of :signup_type,  :in => %w[all noemail social email]
 
   before_create :disallow_javascript
+  before_update :disallow_javascript
   before_save :modify_attributes
   before_create :create_widget_lists
   before_create :set_default_theme
@@ -397,7 +398,7 @@ class Group
 
   def disallow_javascript
     unless self.has_custom_js
-       %w[footer _head _question_help _question_prompt head_tag].each do |key|
+       %w[footer head _question_help _question_prompt head_tag].each do |key|
          value = self.custom_html[key]
          if value.kind_of?(Hash)
            value.each do |k,v|
