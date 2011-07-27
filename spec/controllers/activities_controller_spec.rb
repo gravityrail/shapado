857af8c80c9af99329c940d2f94dad329bd0364e
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2011-07-26 20:42:31 -0500

    Add more specs
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe ActivitiesController do
+  include Devise::TestHelpers
+
+  before(:each) do
+    @group = stub_group
+    @user = User.make(:user)
+    @group.add_member(@user, "owner")
+    stub_authentication @user
+  end
+
+  describe "GET index" do
+    it "assigns all activities as @activities" do
+      activities = @group.activities
+      get :index
+      assigns(:activities).should eq(activities)
+    end
+  end
 end
