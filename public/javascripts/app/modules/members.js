4cd085593476a9eaafbe3ccca34d62ec6e1a103f
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2011-08-16 21:19:10 -0500

    Improve members ui: -Remove form to add a member -Add a live search
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- public/javascripts/app/modules/members.js
+++ public/javascripts/app/modules/members.js
@@ -1,20 +1,14 @@
 var Members = {
   initialize: function(data) {
-    if($('#member_user_id').length){
-      $('#member_user_id').autocomplete({
-        source: "/users/autocomplete_for_user_login.json",
-        minLength: 1,
-        select: function( event, ui ) {
-            $( '#member_user_id' ).val(ui.item.login);
-            return false;
-        }
-      })
-      .data("autocomplete")._renderItem = function( ul, item ) {
-        return $( "<li></li>" )
-          .data( "item.autocomplete", item )
-          .append( "<a>" + item.login + "</a>" )
-          .appendTo( ul );
-      };
-    }
+    $("#filter_members").searcher({ url : "/manage/members.js",
+                              target : $("#members"),
+                              behaviour : "live",
+                              timeout : 500,
+                              extraParams : { 'format' : 'js' },
+                              success: function(data) {
+                                $('.pagination').html(data.pagination)
+                              }
+    });
+    $('.filter_input').hide();
   }
 }
