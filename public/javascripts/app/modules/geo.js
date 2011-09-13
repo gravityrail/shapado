9103ea17c11abf51d45e8094d2a2fa3fefd0afa3
Patrick Aljord <patcito@gmail.com>
2011-09-13 00:12:18 -0500

    better fix for geolocation check

--- public/javascripts/app/modules/geo.js
+++ public/javascripts/app/modules/geo.js
@@ -1,16 +1,15 @@
 var Geo = {
   initialize: function() {
     $(document.body).delegate('#question_title', 'click', Geo.localize);
-    $(document.body).delegate('#add_answer', 'hover', Geo.localize);
     $(document.body).delegate('#new_answer', 'hover', Geo.localize);
   },
   localize: function(){
-    if(!$.cookie('geo')){
-      $.cookie('geo',1);
+    if($('meta[data-geo=1]').length==0){
+      $('body').append('<meta data-geo=1>');
       navigator.geolocation.getCurrentPosition(function(position){
         $('.lat_input').val(position.coords.latitude)
         $('.long_input').val(position.coords.longitude)
-      }, function(){console.log('NO')});
+      }, function(){});
     }
   }
 };
