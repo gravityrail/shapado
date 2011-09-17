556efb187f97c710cb0cd0346a10954aa0c0229c
Patrick Aljord <patcito@gmail.com>
2011-09-16 19:37:01 -0500

    Revert "fix mathjax"
    This reverts commit 7f3f95844eb155981c064e08f0117b5a5a870828.

--- public/javascripts/app/modules/jqmath.js
+++ public/javascripts/app/modules/jqmath.js
@@ -2,7 +2,7 @@ var Jqmath = {
   initialize: function() {
     Modernizr.load([{
       test: $('meta[data-jqmath]').length > 0 && $('.markdown').text().indexOf('$$')>-1,
-      yep: '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+      yep: $.merge($.merge([],eval($('meta[data-jqmath]').attr('data-jsassets'))||[]),eval($('meta[data-jqmath]').attr('data-cssassets'))||[])
     }])
   }
 };
