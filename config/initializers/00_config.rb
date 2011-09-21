48692288ddd2f0dacb91fb0a870b918859d3892f
Patrick Aljord <patcito@gmail.com>
2011-09-20 22:07:18 -0500

    updated js assets handling needs a script to automize the process

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -26,5 +26,5 @@ AppConfig.share_links = share_links[Rails.env]
 if Rails.env == "development" || ENV['debug_assets']
   MODERNIZR = :modernizrdev
 else
-  MODERNIZR = :modernizr
+  MODERNIZR = 'modernizr_1'
 end
