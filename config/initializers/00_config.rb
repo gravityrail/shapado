6328e161b41799a4a20bcc7240f0b6b0a1b45007
Patrick Aljord <patcito@gmail.com>
2011-09-22 20:37:21 -0500

    updated jsassets

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -26,5 +26,5 @@ AppConfig.share_links = share_links[Rails.env]
 if Rails.env == "development" || ENV['debug_assets']
   MODERNIZR = :modernizrdev
 else
-  MODERNIZR = 'modernizr_3'
+  MODERNIZR = 'modernizr_4'
 end
