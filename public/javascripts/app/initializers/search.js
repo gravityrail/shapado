9b076d4f255c5c2429b4f6ed05ded2b03b69a07b
Jorge H. Cuadrado <kuadrosxx@gmail.com>
2011-10-07 12:35:46 -0500

    Search live plugin: restore target's html when query's value is lower than minLength
    Signed-off-by: Jorge H. Cuadrado <kuadrosxx@gmail.com>

--- public/javascripts/app/initializers/search.js
+++ public/javascripts/app/initializers/search.js
@@ -19,6 +19,7 @@
       var settings = options;
       var self = $(this);
       var extraParams = [];
+      var cache = settings.target.html();
 
       if(typeof settings.fields == "undefined") {
         settings.fields = $(this).find("input[type=text],textarea");
@@ -30,14 +31,14 @@
       }
 
       var query = function() {
-      settings.before_query.call(self, settings.target);
+        settings.before_query.call(self, settings.target);
 
-      $.ajax({
-        url: settings.url,
-        dataType: "json",
-        type: "GET",
-        data: $.merge(settings.fields.serializeArray(), extraParams),
-        success: function(data) {
+        $.ajax({
+          url: settings.url,
+          dataType: "json",
+          type: "GET",
+          data: $.merge(settings.fields.serializeArray(), extraParams),
+          success: function(data) {
             settings.target.empty();
             settings.target.append(data.html);
             settings.success(data);
@@ -56,13 +57,17 @@
         $.each(settings.fields, function(){
           var timer = null;
           $(this).keyup(function() {
-            if ($(this).val().length <= settings.minLength) return;
-            if(this.value != last) {
-              if (timer){
-                clearTimeout(timer);
+            if(!($(this).val().length <= settings.minLength)){
+              if(this.value != last) {
+                if (timer){
+                  clearTimeout(timer);
+                }
+                last = this.value;
+                timer = setTimeout(query, settings.timeout);
               }
-              last = this.value;
-              timer = setTimeout(query, settings.timeout);
+            } else {
+              settings.target.empty();
+              settings.target.append(cache);
             }
           });
         });
@@ -71,9 +76,13 @@
       var focusout = function() {
         $.each(settings.fields, function(){
           $(this).blur(function() {
-           if ($(this).val().length <= settings.minLength) return;
-            if(this.value != last) {
-              query();
+            if (!($(this).val().length <= settings.minLength)) {
+              if(this.value != last) {
+                query();
+              }
+            } else {
+              settings.target.empty();
+              settings.target.append(cache);
             }
           });
         });
