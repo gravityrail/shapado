62869883978e728f27ee7da6b5b959558e1852e8
Patrick Aljord <patcito@gmail.com>
2012-02-05 02:03:20 -0500

    smooth scroll to top when clicking on topbar à la G+

--- app/assets/javascripts/app/modules/ui.js
+++ app/assets/javascripts/app/modules/ui.js
@@ -10,7 +10,7 @@ var Ui = {
     });
 
     Ui.initialize_ajax_tooltips();
-
+    Ui.initialize_smooth_scroll_to_top();
     if(Ui.supports_input_placeholder()) {
       $('.hideifplaceholder').remove();
     };
@@ -222,6 +222,14 @@ var Ui = {
       fields.tabs();
     }
   },
+  initialize_smooth_scroll_to_top: function(){
+    $(".top-bar").click(function(e) {
+      var isTopBar = $(e.target).hasClass('top-bar');
+      if(isTopBar)
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+    });
+  }
+  ,
   initialize_ajax_tooltips: function(){
     $(document.body).on("mouseleave",".markdown, .toolbar, .Question, .tag-list, .user-data, .tooltip", function(event) {
       $(".tooltip").hide();
