0cac5c4db6f9919a0f20ac91cf4966f9143da763
Johanna Mantilla Duque <johanna1431@gmail.com>
2012-02-12 14:57:45 -0500

    Remove _setAllowHash of googlean_script - This method is deprecated
    Signed-off-by: Johanna Mantilla Duque <johanna1431@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -309,7 +309,7 @@ module ApplicationHelper
   end
 
   def googlean_script(analytics_id, domain)
-    raw %Q{<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(["_setAccount","#{analytics_id}"]);_gaq.push(["_setDomainName","#{domain}"]);_gaq.push(["_setAllowHash","false"]);_gaq.push(["_trackPageview"]);(function(){var ga=document.createElement("script");ga.type="text/javascript";ga.async=true;ga.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,s)})();</script>}
+    raw %Q{<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(["_setAccount","#{analytics_id}"]);_gaq.push(["_setDomainName","#{domain}"]);_gaq.push(["_trackPageview"]);(function(){var ga=document.createElement("script");ga.type="text/javascript";ga.async=true;ga.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,s)})();</script>}
   end
 
   def logged_out_language_filter
