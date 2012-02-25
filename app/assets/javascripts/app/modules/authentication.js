3e2e1f6b8c1253effa80da1b11ab6625f6b4b4be
Patrick Aljord <patcito@gmail.com>
2012-02-25 00:55:46 -0500

    fix styling of join and sign in notices

--- app/assets/javascripts/app/modules/authentication.js
+++ app/assets/javascripts/app/modules/authentication.js
@@ -11,6 +11,7 @@ var Auth = {
   position_dropdown: function(){
     if(Ui.offline()){
       $('.providers-list').show().offset({left: $('.offline').offset().left+$('.offline').width()-$('.providers-list').width()}).hide();
+        //$('.providers-list').show().offset({left: $('body').width()/2-$('#column2').width()/2}).css({width: '290px'});
     }
   },
   dropdown_toggle: function(){
@@ -44,25 +45,9 @@ var Auth = {
         var title = $('#join_dialog').attr('data-title');
         $('#join_dialog').dialog({title: title, modal: true, resizable: false})
     } else {
-      var changed = $('#login_dialog').find('li a.login').attr('data-changed');
-      if(typeof(join)!='undefined' && changed != 1) {
-        var newhref = $('li a.login').attr('data-href');
-        var oldhref = $('li a.login').attr('href');
-
-        $('#login_dialog').find('li a.login').attr({href: newhref, 'data-href': oldhref, 'data-changed': 1});
-      }
-      if(!title)
-      var title = $('#login_dialog').attr('data-title');
-      $('#login_dialog').dialog({title: title,
-                               modal: true,
-                               width: "250px",
-                               resizable: false,
-                               beforeClose:function(){
-                                 var changed = $('#login_dialog').find('li a.login').attr('data-changed');
-                                 if(changed==1)
-                                 $('#login_dialog').find('li a.login').attr({href: newhref,
-                                                                             'data-href': oldhref, 'data-changed': 0});
-                               }});
-      }
+        $('.offline li:first').trigger('click');
+        Messages.show($('.offline').data('signin-notice'), 'error', 5000 );
+        return false;
+    }
   }
 };
