b10459f397d2a58cde9e1b6dbeea13a68f46bb7b
Jorge Cuadrado <kuadrosxx@gmail.com>
2012-02-27 19:32:59 -0500

    Upadate mongoid and mongoid_ext
    Signed-off-by: Jorge Cuadrado <kuadrosxx@gmail.com>

--- lib/shapado/models/trackable.rb
+++ lib/shapado/models/trackable.rb
@@ -9,91 +9,90 @@ module Shapado
         after_destroy :__on_destroy
       end
 
-      module InstanceMethods
-        private
-        def __generate_trackable_info
-          self.class.__fields_for_class(self, self.class.trackable_opts[:fields])
-        end
+      #InstanceMethods
+      private
+      def __generate_trackable_info
+        self.class.__fields_for_class(self, self.class.trackable_opts[:fields])
+      end
 
-        def __generate_target_info
-          if target = __resolve_target
-            klass = target.class
-            if klass.respond_to?(:trackable_opts)
-              self.class.__fields_for_class(target, klass.trackable_opts[:fields])
-            end
+      def __generate_target_info
+        if target = __resolve_target
+          klass = target.class
+          if klass.respond_to?(:trackable_opts)
+            self.class.__fields_for_class(target, klass.trackable_opts[:fields])
           end
         end
+      end
 
-        def __resolve_trackable_scope
-          scope_fields = self.class.trackable_opts[:scope]
-          scope_fields = [scope_fields] if !scope_fields.is_a?(Array)
+      def __resolve_trackable_scope
+        scope_fields = self.class.trackable_opts[:scope]
+        scope_fields = [scope_fields] if !scope_fields.is_a?(Array)
 
-          scope = {}
-          scope_fields.each do |f|
-            scope[f] = self[f]
-          end
-
-          scope
+        scope = {}
+        scope_fields.each do |f|
+          scope[f] = self[f]
         end
 
-        def __resolve_target
-          if track_in = self.class.trackable_opts[:target]
-            self.send(track_in)
-          end
+        scope
+      end
+
+      def __resolve_target
+        if track_in = self.class.trackable_opts[:target]
+          self.send(track_in)
+        end
+      end
+
+      def __on_create
+        __create_activity("create")
+      end
+
+      def __on_update
+        __create_activity("update")
+      end
+
+      def __on_destroy
+        __create_activity("destroy")
+      end
+
+      def __create_activity(action, opts = {})
+        Rails.logger.info "Adding #{action} activity for #{self.class} with #{opts.inspect}"
+
+        group_id = self[:group_id] || Thread.current[:current_group].try(:id)
+        user_id = Thread.current[:current_user].try(:id) || self[:user_id]
+        target = __resolve_target
+
+        conds = {
+          :action => action,
+          :group_id => group_id,
+          :user_id => user_id,
+          :trackable_id => self.id
+        }
+        if target
+          conds[:target_id] = target.id
         end
 
-        def __on_create
-          __create_activity("create")
-        end
+        callback = self.class.trackable_opts[:callback] ? self.class.trackable_opts[:callback] : nil
+        activity = Activity.where(conds).desc(:created_at).first
 
-        def __on_update
-          __create_activity("update")
-        end
-
-        def __on_destroy
-          __create_activity("destroy")
-        end
-
-        def __create_activity(action, opts = {})
-          Rails.logger.info "Adding #{action} activity for #{self.class} with #{opts.inspect}"
-
-          group_id = self[:group_id] || Thread.current[:current_group].try(:id)
-          user_id = Thread.current[:current_user].try(:id) || self[:user_id]
-          target = __resolve_target
-
-          conds = {
+        if activity
+          callback && callback.call(activity, target||self)
+          activity.increment(:times => 1)
+        else
+          opts.merge!({
             :action => action,
+            :trackable_info => __generate_trackable_info,
+            :target_info => __generate_target_info,
+            :scope => __resolve_trackable_scope,
             :group_id => group_id,
             :user_id => user_id,
-            :trackable_id => self.id
-          }
-          if target
-            conds[:target_id] = target.id
-          end
+            :trackable => self,
+            :target => target,
+            :user_ip => Thread.current[:current_ip]
+          })
 
-          callback = self.class.trackable_opts[:callback] ? self.class.trackable_opts[:callback] : nil
-          activity = Activity.where(conds).desc(:created_at).first
-
-          if activity
-            callback && callback.call(activity, target||self)
-            activity.increment(:times => 1)
-          else
-            opts.merge!({
-              :action => action,
-              :trackable_info => __generate_trackable_info,
-              :target_info => __generate_target_info,
-              :scope => __resolve_trackable_scope,
-              :group_id => group_id,
-              :user_id => user_id,
-              :trackable => self,
-              :target => target,
-              :user_ip => Thread.current[:current_ip]
-            })
-
-            activity = Activity.new(opts)
-            callback && callback.call(activity, target||self)
-            activity.save!
-          end
+          activity = Activity.new(opts)
+          callback && callback.call(activity, target||self)
+          activity.save!
         end
       end
 
