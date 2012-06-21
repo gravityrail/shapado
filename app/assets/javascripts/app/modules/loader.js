90cc3b643d5bc976874323015bd32a9bdb0dad9a
Patrick Aljord <patcito@gmail.com>
2012-06-21 20:54:52 +0100

    remove plans js

--- app/assets/javascripts/app/modules/loader.js
+++ app/assets/javascripts/app/modules/loader.js
@@ -22,8 +22,6 @@ Loader = function() {
     }
     if($body.hasClass("questions-controller")) {
       Questions.initialize($body);
-    } else if($body.hasClass("doc-controller plans application")) {
-      Plans.initialize($body);
     } else if($body.hasClass("widgets-controller")) {
       Widgets.initialize($body);
     } else if($body.hasClass("users-controller")) {
